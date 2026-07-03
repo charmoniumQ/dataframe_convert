2078ee1123d168bd3fc0b20dee2ee9e7af86b48e
Samuel Grayson <sam@samgrayson.me>
2026-07-03 04:06:25 -0500

    Add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+      in
+      {
+        packages.default = craneLib.buildPackage {
+          src = ./.;
+        };
+      }
+    );
+}
